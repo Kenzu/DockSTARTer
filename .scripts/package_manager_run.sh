9d99a486dd2c7ba5022d9af126c311e604b78f9f
Jesper Lindeberg <jlindeberg@gmail.com>
2019-11-12 23:17:36 +0100

    apt/zypper conflict fix (maybe)

--- .scripts/package_manager_run.sh
+++ .scripts/package_manager_run.sh
@@ -10,7 +10,7 @@ package_manager_run() {
         run_script "pm_dnf_${ACTION}"
     elif [[ -n "$(command -v yum)" ]]; then
         run_script "pm_yum_${ACTION}"
-    elif [[ -n "$(command -v apt-get)" ]]; then
+    elif [[ -n "$(command -v apt-get)" && ! -n "$(command -v zypper)" ]]; then
         run_script "pm_apt_${ACTION}"
     else
         fatal "Package manager not detected!"
