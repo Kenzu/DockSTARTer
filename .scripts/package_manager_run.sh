4b13745fea6cb4d85b90b881d9ad19dc02b76c2d
Jesper Lindeberg <jlindeberg@gmail.com>
2019-11-12 22:48:34 +0100

    Fix for apt alias on openSUSE

--- .scripts/package_manager_run.sh
+++ .scripts/package_manager_run.sh
@@ -4,14 +4,14 @@ IFS=$'\n\t'
 
 package_manager_run() {
     local ACTION=${1:-}
-    if [[ -n "$(command -v apt-get)" ]]; then
-        run_script "pm_apt_${ACTION}"
+    if [[ -n "$(command -v zypper)" ]]; then
+        run_script "pm_zypper_${ACTION}"
     elif [[ -n "$(command -v dnf)" ]]; then
         run_script "pm_dnf_${ACTION}"
     elif [[ -n "$(command -v yum)" ]]; then
         run_script "pm_yum_${ACTION}"
-    elif [[ -n "$(command -v zypper)" ]]; then
-        run_script "pm_zypper_${ACTION}"
+    elif [[ -n "$(command -v apt-get)" ]]; then
+        run_script "pm_apt_${ACTION}"
     else
         fatal "Package manager not detected!"
     fi
